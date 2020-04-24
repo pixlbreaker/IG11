4fec04859873ff539f63ab074899de0878b7605f
Mike Skotar <michaelskotar@gmail.com>
2020-04-24 19:07:06 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import os
+import json
+
+from discord.ext import commands
+
+f = open("login.json")
+data = json.load(f)
+TOKEN = data['TOKEN']
+
+bot = commands.Bot(command_prefix="!")
+
+# Load function for extensions
+@bot.command
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+# Unloads function for extension
+@bot.command
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+# Run the bot
+bot.run(TOKEN)
