3cd94914e17e345715d7c4db8b896e7c971a4079
Mike Skotar <michaelskotar@gmail.com>
2020-04-29 21:35:30 -0400

    Finished the audio clip part

--- cogs/soundboard.py
+++ cogs/soundboard.py
@@ -1,6 +1,7 @@
 import discord
 import pafy
 import youtube_dl
+import os
 from discord.ext import tasks, commands
 from discord import FFmpegPCMAudio
 from discord.utils import get
@@ -15,31 +16,20 @@ class Soundboard(commands.Cog):
         self.bot = bot
         #self.play_queue.start()
     
-    def __playaudio__(self, ctx, audio_path):
+    async def __playaudio__(self, ctx, audio_path):
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
         guild = ctx.guild
         voice_client: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=guild)
         audio_source = discord.FFmpegPCMAudio(audio_path)
 
-        #self.songs.append(audio_source)
-        if not voice_client.is_playing():
-            voice_client.play(audio_source, after=None)
-        else:
-            voice_client.pause()
-            voice_client.play(audio_source, after=None)
+        await ctx.send("Playing audio")
+        await voice_client.play(audio_source, after=None)
     
-    def __join__(self, ctx):
-        """Joins the users channel"""
-        try:
-            channel = ctx.message.author.voice.channel
-            voice = get(self.bot.voice_clients, guild=ctx.guild)
-            if voice and voice.is_connected():
-                voice.move_to(channel)
-                ctx.send("Moved channel")
-            else:
-                voice = channel.connect()
-                ctx.send("Joined channel")
-        except:
-            ctx.send("You are not connected to a channel")
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -64,8 +54,28 @@ class Soundboard(commands.Cog):
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild=ctx.guild)
         audio_path = "sound/bitconnect.mp3"
-        self.__join__(ctx)
-        self.__playaudio__(ctx, audio_path)
+        await self.__playaudio__(ctx, audio_path)
+
+    @commands.command(pass_context=True)
+    async def p(self, ctx, name):
+        """
+        """
+        l = os.listdir('./sound')
+        li=[x.split('.')[0] for x in l]
+        i = li.index(name)
+        audio_path = "sound/" + l[i]
+        await self.__playaudio__(ctx, audio_path)
+        
+
+    
+    @commands.command(pass_context=True,)
+    async def soundlist(self, ctx):
+        """
+        """
+        await ctx.send("This is a list of all the avaiable sounds")
+        for filename in os.listdir('./sound'):
+            await ctx.send(filename)
+        
         
     
     # @commands.command(pass_context=True)
